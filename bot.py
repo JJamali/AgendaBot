2855db2a1311d33f07b05567b20b40ef9d7f7112
Lukas Boelling <lukasboelling002@gmail.com>
2020-11-14 17:16:31 -0500

    Add bot function to insert deadline

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import MySQLdb
 from discord.ext import commands
 
 from deadlines import Deadlines
@@ -8,7 +9,12 @@ from deadlines import Deadlines
 class CalendarBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.add_cog(Deadlines())
+        self.db = MySQLdb.connect(user=os.getenv('db_user'),
+                                  password=os.getenv('db_password'),
+                                  host='localhost',
+                                  database='deadlinedb')
+        self.cursor = self.db.cursor()
+        self.add_cog(Deadlines(self.db, self.cursor))
 
     async def on_ready(self):
         print('We have logged in as {0.user}'.format(self))
--- deadlines.py
+++ deadlines.py
@@ -3,9 +3,25 @@ from discord.ext import commands
 
 
 class Deadlines(commands.Cog):
-    def __init__(self):
-        pass
+    def __init__(self, db, cursor):
+        self.db = db
+        self.cursor = cursor
 
     @commands.command()
     async def hello(self, ctx):
         await ctx.send('Hello!')
+
+    @commands.command(name='new')
+    async def new_deadline(self, ctx, argument):
+        print(argument)
+        department, course_num, name, due_date = argument.split(',')
+        guild_id = ctx.message.guild.id
+
+        self.insert_deadline(guild_id, department, course_num, name, due_date)
+        print('done')
+
+    def insert_deadline(self, guild_id, department, course_num, name, due_date):
+        self.cursor.execute("INSERT INTO deadlines VALUES("
+                            "%s, %s, %s, %s, %s"
+                            ")", (guild_id, department, course_num, name, due_date))
+        self.db.commit()
