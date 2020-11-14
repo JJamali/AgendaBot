d66d708ff5aaeb8638a4b08a348c7a879e6d92b0
Lukas Boelling <lukasboelling002@gmail.com>
2020-11-14 15:55:33 -0500

    Initialize database

---
+++ init_database.py
@@ -0,0 +1,26 @@
+import MySQLdb
+import os
+
+
+def init_database(user, password):
+    db = MySQLdb.connect(user=user, password=password, host='localhost')
+    cursor = db.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS deadlinedb")
+
+    db.select_db('deadlinedb')
+    cursor = db.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS deadlines ("
+                   "    guild_id BIGINT,"
+                   "    department VARCHAR(20),"
+                   "    course_num VARCHAR(20),"
+                   "    name VARCHAR(20),"
+                   "    due_date DATETIME"
+                   ")")
+    db.close()
+
+
+if __name__ == '__main__':
+    USER = os.getenv('db_user')
+    PASSWORD = os.getenv('db_password')
+    init_database(USER, PASSWORD)
