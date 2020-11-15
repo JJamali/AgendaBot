35189972fd5c7b9346456ee4a421a8233f2cb70d
Jordan Jamali <jordanjjamali@gmail.com>
2020-11-15 15:17:45 -0500

    Add help command

--- bot.py
+++ bot.py
@@ -6,6 +6,20 @@ from discord.ext import commands
 from deadlines import Deadlines
 from events import Events
 
+@commands.command()
+async def help(ctx):
+    embed=discord.Embed(color=0x1bd0b2)
+    embed.add_field(name="newdeadline", value="adds a deadline with: `$newdeadline [course], [name], [date]`",
+                    inline=False)
+    embed.add_field(name="removedeadline", value="removes a deadline with: `$removedeadline[index]`", inline=False)
+    embed.add_field(name="cleardeadlines", value="clears all deadlines", inline=False)
+    embed.add_field(name="listdeadlines", value="lists all deadlines", inline=False)
+    embed.add_field(name="newevent", value="adds an event with: `$newevent, [title], [description], [time]`",
+                    inline=False)
+    embed.add_field(name="removeevent", value="removes an event with: `$removeevent[index]`", inline=False)
+    embed.add_field(name="clearevents", value="clears all events", inline=False)
+    embed.add_field(name="listevents", value="lists all events", inline=False)
+    await ctx.send(embed=embed)
 
 class CalendarBot(commands.Bot):
     def __init__(self, *args, **kwargs):
@@ -18,7 +32,8 @@ class CalendarBot(commands.Bot):
 
         self.add_cog(Deadlines(self, self.db, self.cursor))
         self.add_cog(Events(self, self.db, self.cursor))
-
+        self.remove_command('help')
+        self.add_command(help)
     async def on_ready(self):
         print('We have logged in as {0.user}'.format(self))
 
