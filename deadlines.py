15286a454cd39bdefa42571780cb580b071c18ca
Max Ning <maxning24@gmail.com>
2020-11-14 19:04:14 -0500

    Delete deadline functionality added

--- deadlines.py
+++ deadlines.py
@@ -20,8 +20,23 @@ class Deadlines(commands.Cog):
         self.insert_deadline(guild_id, department, course_num, name, due_date)
         print('done')
 
+    @commands.command(name='remove')
+    async def remove_deadline(self, ctx, argument):
+        department, course_num, name, due_date = argument.split(',')
+        guild_id = ctx.message.guild.id
+
+        self.delete_deadline(guild_id, department, course_num, name, due_date)
+        print('delete done')
+
     def insert_deadline(self, guild_id, department, course_num, name, due_date):
         self.cursor.execute("INSERT INTO deadlines VALUES("
                             "%s, %s, %s, %s, %s"
                             ")", (guild_id, department, course_num, name, due_date))
+        self.db.commit()
+
+    def delete_deadline(self, guild_id, department, course_num, name, due_date):
+        self.cursor.execute("DELETE FROM deadlines WHERE "
+                            "guild_id = %s AND department = %s AND course_num = %s AND name = %s AND due_date=%s",
+                            (guild_id, department, course_num, name, due_date))
+
         self.db.commit()
